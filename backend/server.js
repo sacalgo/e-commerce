f50235bc0704b72c9f5555872c5beb42da4f7d11
Sachin Toppa <algorithms2019@gmail.com>
2021-04-21 00:47:23 +0530

    created Schemas for users, order, and product

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,31 @@
-import  express from 'express';
-import  dotenv from 'dotenv';
-import colors from 'colors';
-import connectDB from './config/db.js';
-import  products from './data/products.js';
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
 dotenv.config();
 
 connectDB();
 
-const app=express();
+const app = express();
 
-app.get('/', (req, res)=>{
-    res.send('API is running ...');
+app.get("/", (req, res) => {
+  res.send("API is running ...");
 });
 
-app.get('/api/products', (req, res)=>{
-    res.json(products);
+app.get("/api/products", (req, res) => {
+  res.json(products);
 });
 app.get("/api/products/:id", (req, res) => {
-      const product=products.find(p=>p._id===req.params.id);
-    res.json(product);
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
 });
 
-const PORT=process.env.PORT||6001
+const PORT = process.env.PORT || 6001;
 
-app.listen(6001, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+app.listen(
+  6001,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
