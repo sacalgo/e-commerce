bd8bcb2f2743c67e816ac7a51fc2f299bbc8362c
Sachin Toppa <algorithms2019@gmail.com>
2021-05-05 13:12:20 +0530

    frontend setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* eslint-disable no-unused-vars */
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import "./bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {productListReducer} from './reducers/productReducers';
+
+const reducer = combineReducers({
+  productList:productListReducer
+});
+const initialState = {};
+const middleware = [thunk];
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+export default store;
