06b59a29a392aa0b1dfd8df4d297bb775315b263
Sachin Toppa <algorithms2019@gmail.com>
2021-05-05 14:07:25 +0530

    added Loader and Error Message

---
+++ frontend/src/components/Loader.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Spinner} from 'react-bootstrap';
+const Loader = () => {
+    return (
+        <Spinner animation='border' role='status' style={{
+            width:'100px',
+            margin:'auto',
+            height:'100px',
+            display:'block'
+        }}>
+            <span  class='sr-only'>Loading...</span>
+            
+        </Spinner>
+    )
+}
+
+export default Loader
