63a3c7b0ea0ef1f15e274232e0a81c0ee9cb3286
Sachin Toppa <algorithms2019@gmail.com>
2021-05-07 00:55:40 +0530

    added controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,11 @@
 import express from "express";
-import mongoose from "mongoose";
-import asyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
 const router = express.Router();
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js";
 
-//@desc Fetch all Products
-//@route GET /api/products
-//@access Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-//@desc Fetch sing Product
-//@route GET /api/products/:id
-//@access Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not Found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
