57ba4a6f5cfd4b59dec74a1aa1d3801d6d78bfc6
Sachin Toppa <algorithms2019@gmail.com>
2021-05-07 04:16:26 +0530

    fixed bugs

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,10 +1,10 @@
 
 import jwt from "jsonwebtoken";
-import User from "../models/userModel";
+import User from "../models/userModel.js";
 
 const protect = async (req, res, next) => {
    console.log('something');
    next();
 };
 
-export default protect;
+export default protect
