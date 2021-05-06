5b8272ddac98e444e5e4f8799cfc0cb5810affd6
Sachin Toppa <algorithms2019@gmail.com>
2021-05-06 10:58:34 +0530

    added cart reducer and add cart feature

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+import { CART_ADD_ITEM } from "../constants/cartConstants";
+
+//Thunk
+//we will use  getstate for retrieving from reducers/stores
+export const autoCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`);
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  });
+  //we can only strore strings in localStorage, we can't store json objects
+  //while retrievation we will use JSON.parse()
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
