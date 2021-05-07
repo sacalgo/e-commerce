e6df9bae130252983f9be6bb1cfac7dcda5737fb
Sachin Toppa <algorithms2019@gmail.com>
2021-05-07 06:32:52 +0530

    registeration and password ecryption in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,23 +26,55 @@ const authUser = asyncHandler(async (req, res) => {
   res.send({ email, password });
 });
 
+//@desc  Register a new user
+//@route POST /api/users
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(ResponseStatus.BAD_REQUEST);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(ResponseStatus.CREATED).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(ResponseStatus.BAD_REQUEST);
+    throw new Error("Invalid user data");
+  }
+});
+
 //@desc  Get user profile
 //@route GET /api/users/profile
 //@access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-     const user=await User.findById(req.user._id);
-     if(user){
-       res.json({
-         _id: user._id,
-         name: user.name,
-         email: user.email,
-         isAdmin: user.isAdmin,
-       });
-              
-     }else{
-       res.status(ResponseStatus.NOT_FOUND);
-       throw new Error('User not found');
-     }
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(ResponseStatus.NOT_FOUND);
+    throw new Error("User not found");
+  }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
--- backend/utils/responseStatus.js
+++ backend/utils/responseStatus.js
@@ -1,5 +1,6 @@
 const ResponseStatus = {
   SUCCESS: 200,
+  CREATED: 201,
   BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
   FORBIDDEN: 403,
