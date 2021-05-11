83547d43ed3b8fe5c7aae35cf65a586d21b8467a
Sachin Toppa <algorithms2019@gmail.com>
2021-05-12 02:48:57 +0530

    added order actions and reducers for fetching order details

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,8 +13,10 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from "./reducers/userReducers";
-import { orderCreateReducer } from './reducers/orderReducers';
-
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+} from "./reducers/orderReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -24,7 +26,8 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
-  orderCreate:orderCreateReducer
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
