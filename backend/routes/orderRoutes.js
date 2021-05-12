9cc0ceed6e5c941f65b6ee1059557f23cf1aa570
Sachin Toppa <algorithms2019@gmail.com>
2021-05-12 21:55:37 +0530

    added order retrieval in endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,10 +4,12 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getMyOrders
 } from "../controllers/orderController";
 import protect from "../middleware/authMiddleware";
 
 router.route("/").post(protect, addOrderItems);
+router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
 
