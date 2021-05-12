444102689adc26f70d51637bfb92fad3b2429200
Sachin Toppa <algorithms2019@gmail.com>
2021-05-13 02:44:09 +0530

    delete user funtionality completed on UI

--- frontend/src/screens/UsersListScreen.js
+++ frontend/src/screens/UsersListScreen.js
@@ -5,29 +5,33 @@ import { Table, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { listUsers } from "../actions/userActions";
+import { listUsers, deleteUser } from "../actions/userActions";
 
-const UsersListScreen = ({history}) => {
+const UsersListScreen = ({ history }) => {
   const dispatch = useDispatch();
 
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
 
   const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo} = userLogin;
+  const { userInfo } = userLogin;
+
+  const userDelete = useSelector((state) => state.userDelete);
+  const { success: successDelete } = userDelete;
 
   useEffect(() => {
-      if(userInfo&&userInfo.isAdmin){
-          dispatch(listUsers());
-      }else {
-          history.push('/login');
-      }
-        
-  }, [dispatch, history, userInfo]); 
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      history.push("/login");
+    }
+  }, [dispatch, history, userInfo, successDelete]);
 
-  const deleteHandler=(id)=>{
-      console.log('delete');
-  }
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(deleteUser(id));
+    }
+  };
 
   return (
     <>
