d1b4dcd82238aa32f951fac7116114ee2938e307
Sachin Toppa <algorithms2019@gmail.com>
2021-05-13 12:49:37 +0530

    delete products functionality added at Endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,4 +24,18 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+//@desc Delete a product
+//@route DELETE /api/products/:id
+//@access Private / Admin only
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed" });
+  } else {
+    res.status(ResponseStatus.NOT_FOUND);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductById , deleteProduct};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 import {
   getProductById,
   getProducts,
+  deleteProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware";
 
 router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
