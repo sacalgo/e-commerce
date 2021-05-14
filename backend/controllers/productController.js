c4a950c0a5ed58fe2aad812f19369a4bdcf62c62
Sachin Toppa <algorithms2019@gmail.com>
2021-05-14 16:44:14 +0530

    get top rated products routes and controller added at Endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,10 +6,8 @@ import ResponseStatus from "../utils/responseStatus";
 //@route GET /api/products
 //@access Public
 const getProducts = asyncHandler(async (req, res) => {
-    
-  const pageSize=8;
-  const page=Number(req.query.pageNumber)|| 1;
-
+  const pageSize = 8;
+  const page = Number(req.query.pageNumber) || 1;
 
   const keyword = req.query.keyword
     ? {
@@ -20,10 +18,12 @@ const getProducts = asyncHandler(async (req, res) => {
       }
     : {};
 
-  const count =await Product.countDocuments({...keyword});
+  const count = await Product.countDocuments({ ...keyword });
 
-  const products = await Product.find({ ...keyword }).limit(pageSize).skip(pageSize*(page-1));
-  res.json({products, page, pages:Math.ceil(count/pageSize)});
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 //@desc Fetch sing Product
@@ -133,6 +133,15 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc Get top rated Products
+//@route GET  /api/producst/top
+//@access Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
+
 export {
   getProducts,
   getProductById,
@@ -140,4 +149,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 };
