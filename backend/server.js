7f50c441a60028cddfa2b9398a993ee259a4a6b5
Sachin Toppa <algorithms2019@gmail.com>
2021-05-14 19:10:55 +0530

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,6 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json()); //json data in body
 
-app.get("/", (req, res) => {
-  res.send("API is running ...");
-});
-
 //routes
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -38,6 +34,18 @@ app.get("/api/config/paypal", (req, res) =>
 //es module  accesible
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get(`*`, (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running ...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
