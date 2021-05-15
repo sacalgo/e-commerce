598473c223071e65c5ce280165d8caed2c45b711
Sachin Toppa <algorithms2019@gmail.com>
2021-05-15 14:22:15 +0530

    fixed bugs of toprated product links

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -25,7 +25,7 @@ const ProductCarousel = () => {
     <Carousel pause="hover" className="bg-dark">
       {products.map((product) => (
         <Carousel.Item key={product._id}>
-          <Link to={`/product._id`}>
+          <Link to={`/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
             <Carousel.Caption className="carousel-caption">
               <h2>
