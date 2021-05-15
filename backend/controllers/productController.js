5bca6eca35abf9ebf1378f773cc05156a70591c9
Sachin Toppa <algorithms2019@gmail.com>
2021-05-15 14:43:56 +0530

    pagination lowered

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,7 +6,7 @@ import ResponseStatus from "../utils/responseStatus";
 //@route GET /api/products
 //@access Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 8;
+  const pageSize = 4;
   const page = Number(req.query.pageNumber) || 1;
 
   const keyword = req.query.keyword
